f9e353486b3cf5f232ea4b6ab13e5dba268cb7d2
Jang Soobeen <youdew27@gmail.com>
2026-02-06 17:20:41 +0900

    fix: strict모드 빼야 카카오 로그인됨

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,11 @@ import App from './App';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>,
+  // </React.StrictMode>,
 );
